c2c4a2fea058a3f3ed9b812e7b22990a45e9d72c
Nikaa001 <nikatitilokashvili77@gmail.com>
2023-09-15 22:32:44 +0400

    feat: Game Logic

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# memory-game
+# Hackuna Matata - Collab Project
+
+Memory Games created by refernece given in Figma for BitCamp Collab Project
+
+
+## Usage
+
+- User can ;
+- Registered user has an ability to add their social links to their profile;
+- Registered user can edit their profile details and upload profile photo;
+- Profile can be previewed by Registered User;
+- All data is being sorted in local storage;
+
+### Getting Started
+
+1. First of all you need to clone app repository from github:
+
+   ```
+   git clone git@github.com:beqa200/hackuna-matata.git
+   ```
+
+2. Next step requires to start live server.
+
+## Links
+
+- Solution URL: [Github](https://github.com/beqa200/hackuna-matata)
+- Live Site URL: [Vercel](https://hackuna-matata.vercel.app/)
+
+## Mentor
+Beqa Maisuradze (https://github.com/beqa200)
+
+## Authors
+
+- Nika Titilokashvili (https://github.com/Nikaaa001)
+- Tamo Mchedlishvili (https://github.com/CyberWitch0)
+- Nika Kishko  (https://github.com/Kishk0)
--- pages/play/main.js
+++ pages/play/main.js
@@ -2,8 +2,15 @@ const storedSettings = JSON.parse(localStorage.getItem("parameters"));
 let main = document.querySelector('main');
 let body = document.querySelector('body');
 
+let arr = [];
+
 if (storedSettings && (storedSettings.gridSize === "4x4")) {
 
+    arr = shuffleArray(Array.from({
+        length: 8
+    }, (_, index) => index + 1));
+
+    console.log(arr)
     let cards = "";
     for (let i = 1; i < 17; i++) {
         cards += `<div class= "num ${i}"></div>`
@@ -11,6 +18,10 @@ if (storedSettings && (storedSettings.gridSize === "4x4")) {
 
     main.insertAdjacentHTML("beforeend", cards);
 } else {
+
+    arr = shuffleArray(Array.from({
+        length: 18
+    }, (_, index) => index + 1));
 
     body.classList.add("grid6x6")
     let cards = "";
@@ -20,23 +31,84 @@ if (storedSettings && (storedSettings.gridSize === "4x4")) {
     main.insertAdjacentHTML("beforeend", cards);
 }
 
+
+
+
+
+
+const cards = Array.from(document.querySelectorAll(".num"));
+
+let temparr = [];
+
+let moves = 0;
+
+if (storedSettings.theme == "Numbers") {
+    cards.map((item, index) => {
+        item.addEventListener('click', () => {
+            item.style.background = '#BCCED9';
+            item.innerText = arr[index];
+            temparr.push(item);
+            console.log(temparr);
+            if (temparr.length == 2) {
+                moves++;
+                document.querySelector(".number").textContent = moves.toString().padStart(2, 0);
+                if (temparr[0].innerText != temparr[1].innerText) {
+                    cards.map((item) => {
+                        item.style.pointerEvents = "none";
+                    })
+                    setTimeout(() => { 
+                        temparr[0].innerText = "";
+                        temparr[1].innerText = "";
+                        temparr[0].style.background = '#304859';
+                        temparr[1].style.background = '#304859';
+                        temparr = [];
+                        cards.map((item) => {
+                            if (item.innerText == "") {
+                            item.style.pointerEvents = "auto";
+                            // num = document.querySelector('.num').style.background = "#FDA214";
+                            }
+                        })
+                    }, 1200)
+                } else {
+                    temparr[0].style.background = '#FDA214';
+                    temparr[1].style.background = '#FDA214';
+                    temparr = [];
+                }
+            }
+        })
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const timerElement = document.getElementById("timer");
 
 let seconds = 0;
 let minutes = 0;
 
 function updateTimer() {
-  seconds++;
+    seconds++;
 
-  if (seconds === 60) {
-    seconds = 0;
-    minutes++;
-  }
+    if (seconds === 60) {
+        seconds = 0;
+        minutes++;
+    }
 
-  const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
-  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
 
-  timerElement.textContent = `${formattedMinutes}:${formattedSeconds}`;
+    timerElement.textContent = `${formattedMinutes}:${formattedSeconds}`;
 }
 
 const timerInterval = setInterval(updateTimer, 1000);
@@ -47,6 +119,25 @@ let menu = document.querySelector('.menu-box');
 let mainMenu = document.querySelector('#mainMenu');
 
 menu.addEventListener('click', () => {
-        mainMenu.style.display = 'flex';
+    mainMenu.style.display = 'flex';
 })
 
+
+
+
+function shuffleArray(array) {
+
+
+    // Duplicate the array
+    const repeatedArray = array.concat(array);
+
+    for (let i = repeatedArray.length - 1; i > 0; i--) {
+        // Generate a random index between 0 and i (inclusive)
+        const j = Math.floor(Math.random() * (i + 1));
+
+        // Swap array[i] and array[j]
+        [repeatedArray[i], repeatedArray[j]] = [repeatedArray[j], repeatedArray[i]];
+    }
+
+    return repeatedArray;
+}
