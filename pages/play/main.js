0672a382e528baf258337bf2fd9f20bfb76b68b2
Nikaa001 <nikatitilokashvili77@gmail.com>
2023-09-15 20:10:19 +0400

    feat: make timer

--- pages/play/main.js
+++ pages/play/main.js
@@ -19,3 +19,34 @@ if (storedSettings && (storedSettings.gridSize === "4x4")) {
     }
     main.insertAdjacentHTML("beforeend", cards);
 }
+
+const timerElement = document.getElementById("timer");
+
+let seconds = 0;
+let minutes = 0;
+
+function updateTimer() {
+  seconds++;
+
+  if (seconds === 60) {
+    seconds = 0;
+    minutes++;
+  }
+
+  const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+
+  timerElement.textContent = `${formattedMinutes}:${formattedSeconds}`;
+}
+
+const timerInterval = setInterval(updateTimer, 1000);
+
+
+
+let menu = document.querySelector('.menu-box');
+let mainMenu = document.querySelector('#mainMenu');
+
+menu.addEventListener('click', () => {
+        mainMenu.style.display = 'flex';
+})
+
